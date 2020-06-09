c7c8decbfa9eb4cd2fa7e59f9d05a5ca88a8c23e
reksus <mail2ttarun@gmail.com>
2020-06-09 15:59:43 +0530

    Updates

---
+++ precache-manifest.db8681c66866f9598a79e0c0e8f3698b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c82e3560c0891f6b8df26df2e37424f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac911d6233f517928509",
+    "url": "/static/css/main.7daade7b.chunk.css"
+  },
+  {
+    "revision": "a8308874f7819293d16e",
+    "url": "/static/js/2.4346dbfd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4346dbfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac911d6233f517928509",
+    "url": "/static/js/main.3c276968.chunk.js"
+  },
+  {
+    "revision": "37a0d35451170c581fd7",
+    "url": "/static/js/runtime-main.c1487c63.js"
+  }
+]);
